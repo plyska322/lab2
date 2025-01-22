a633efa5073a4ef6ed4a68d97c256df1a06a1e6e
plyska322 <161023952+plyska322@users.noreply.github.com>
2025-01-22 16:11:47 +0200

    Add files via upload

---
+++ include/calculator.h
@@ -0,0 +1,13 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+#include <iostream>
+class Calculator
+{
+    public:
+        double Add (double, double);
+	double Sub (double, double);
+        double Mul (double, double);
+	void PrintResult(double a, double b);
+};
+
+#endif//CALCULATOR_H
